5edc1bdcae486dfb53eef76a9bb194424a209416
DGriffin91 <github@dgdigital.net>
2024-02-16 10:53:28 -0800

    bevy13

--- src/main.rs
+++ src/main.rs
@@ -215,6 +215,7 @@ pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             EnvironmentMapLight {
                 diffuse_map: asset_server.load("environment_maps/pisa_diffuse_rgb9e5_zstd.ktx2"),
                 specular_map: asset_server.load("environment_maps/pisa_specular_rgb9e5_zstd.ktx2"),
+                intensity: 250.0,
             },
             CameraController::default().print_controls(),
         ))
